792016c55cd56288ee99b4c75f3c60c47ba64614
Arzoo <115339667+desireeye@users.noreply.github.com>
2023-08-17 11:29:19 +0530

    Add files via upload

---
+++ structure.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+struct rectangle{
+int length;
+int breadth;
+
+};
+int main(){
+	struct rectangle r;
+	r.length=24;
+	r.breadth=686;
+	cout<<r.length<<endl;
+	cout<<r.breadth<<endl;
+
+}
+
